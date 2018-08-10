8f0b0e023733e9545ed8bacd78ca3a66d094094b
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-08-10 08:29:16 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  
+  # Create a route that is mapped to a students' controller index action, the
+  # resulting route should be /students
+  resources :students, only: :index
+  # List all routes with "rails routes":
+  # https://guides.rubyonrails.org/command_line.html#routes
+  # If that doesn't work, use "rake routes": https://stackoverflow.com/questions/6798521/rails-3-i-want-to-list-all-paths-defined-in-my-rails-application
 end
